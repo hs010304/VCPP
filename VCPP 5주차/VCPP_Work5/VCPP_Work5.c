bfb620e346a3c4698ad583cd08cc3ba789a4a00f
hs010304 <yyhhss0304@naver.com>
2023-10-16 20:10:19 +0900

    [HS]6Week_Commit

--- VCPP 5주차/VCPP_Work5/VCPP_Work5.c
+++ VCPP 5주차/VCPP_Work5/VCPP_Work5.c
@@ -1,169 +1,171 @@
-﻿#include <windows.h>
+﻿#ifdef UNICODE
+#pragma comment(linker, "/entry:wWinMainCRTStartup /subsystem:console")
+#else
+#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
+#endif
 
-#define WINDOW_TITLE TEXT("202007032 윤형식")
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
+#include <windows.h>
 
+// 그림 그리기 시작점
 POINT startPoint;
-BOOL Draw = FALSE;
-BOOL Move = FALSE;
-HWND hwnd;                       //핸들러
-HBRUSH PinkBrush, WhiteBrush;
+BOOL isDrawing = FALSE; // 그림 그리기 모드인지 여부
+BOOL isMoving = FALSE;  // 그림 이동 모드인지 여부
+HWND hwnd;              // 윈도우 핸들
+HBRUSH blueBrush, whiteBrush; // 파란색과 흰색 브러시
 
-int Move_X, Move_Y; // 이전 마우스 위치, 현재 마우스 위치 차이로 사각형 이동시 필요한 값 구하기
-int DrawStart_X, DrawStart_Y, DrawEnd_X, DrawEnd_Y; // 이전 사각형 시작점, 끝점을 저장후 사각형 삭제후 새로운 사각형 그리기 위함
+int moveOffsetX, moveOffsetY;  // 그림 이동 시 시작점과 마우스 포인터의 차이
+int lastDrawStartX, lastDrawStartY, lastDrawEndX, lastDrawEndY; // 마지막으로 그린 사각형의 좌표
 
-void Cleanup() {    // 리소스 해제 함수 (메모리 누수 방지)
-    if (PinkBrush) {
-        DeleteObject(PinkBrush);
-        PinkBrush = NULL;
-    }
-    if (WhiteBrush) {
-        DeleteObject(WhiteBrush);
-        WhiteBrush = NULL;
-    }
-}
-
-
+// 윈도우 프로시저 함수
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    switch (uMsg) {
-        // 브러쉬 생성 및 Draw, Move 변수 초기화
-    case WM_CREATE:
-        WhiteBrush = CreateSolidBrush(RGB(255, 255, 255));
-        PinkBrush = CreateSolidBrush(RGB(255, 0, 255));
-        Move = FALSE;
-        Draw = FALSE;
-        Move_X = 0;
-        Move_Y = 0;
-        DrawStart_X = 0;
-        DrawStart_Y = 0;
-        DrawEnd_X = 0;
-        DrawEnd_Y = 0;
-        break;
+    LRESULT result = 0;
 
-    case WM_LBUTTONDOWN:    // 왼쪽 버튼 눌렀을시 Draw 활성화, 현재 마우스 좌표 저장
-        Draw = TRUE;
+    if (uMsg == WM_CREATE) {
+        // 초기화
+        whiteBrush = CreateSolidBrush(RGB(255, 255, 255)); // 흰색 브러시 생성
+        blueBrush = CreateSolidBrush(RGB(0, 0, 255));      // 파란색 브러시 생성
+        isMoving = FALSE;
+        isDrawing = FALSE;
+        moveOffsetX = 0;
+        moveOffsetY = 0;
+        lastDrawStartX = 0;
+        lastDrawStartY = 0;
+        lastDrawEndX = 0;
+        lastDrawEndY = 0;
+    }
+    else if (uMsg == WM_LBUTTONDOWN) {
+        // 왼쪽 마우스 버튼 클릭 시 그림 그리기 시작
+        isDrawing = TRUE;
         startPoint.x = LOWORD(lParam);
         startPoint.y = HIWORD(lParam);
-        break;
-
-    case WM_LBUTTONUP:
-        if (Draw) {
-            Draw = FALSE;
-            int end_X = LOWORD(lParam);
-            int end_Y = HIWORD(lParam);
-            HDC hdc = GetDC(hwnd);          //  hdc 가져오기
-            SelectObject(hdc, WhiteBrush);
-            SetROP2(hdc, R2_WHITE);         //  흰색 사용 하도록 설정 덮어씌우기
-            Rectangle(hdc, DrawStart_X, DrawStart_Y, DrawEnd_X, DrawEnd_Y); //  함수 호출하여 흰색 사각형을 그림 (덮어씌우기)
-            SetROP2(hdc, R2_COPYPEN);       // 픽셀 지정색으로 칠하기
-            SelectObject(hdc, PinkBrush);
-            Rectangle(hdc, startPoint.x, startPoint.y, end_X, end_Y); //  함수 호출하여 분홍색 사각형 그림 
-            ReleaseDC(hwnd, hdc);           // dc 해제 메모리 누수 방지 
-            DrawStart_X = startPoint.x;     // 이전 사각형의 x,y 좌표를 현재 좌표로 변경
-            DrawStart_Y = startPoint.y;
-            DrawEnd_X = end_X;
-            DrawEnd_Y = end_Y;
-        }
-        break;
-
-    case WM_RBUTTONDOWN:    //  오른쪽 버튼 눌렀을시 현재 마우스 위치가 이전에 그려진 사각형 위에 있는지 확인,
-        //  조건 충족시 Move 활성화 현재 마우스 위치와 이전 사각형 시작점 사이의 X, Y 좌표 저장
-        if (LOWORD(lParam) >= DrawStart_X && LOWORD(lParam) <= DrawEnd_X &&
-            HIWORD(lParam) >= DrawStart_Y && HIWORD(lParam) <= DrawEnd_Y) {
-            Move = TRUE;
-            Move_X = LOWORD(lParam) - DrawStart_X;
-            Move_Y = HIWORD(lParam) - DrawStart_Y;
-        }
-        break;
-
-    case WM_RBUTTONUP:      // 오른쪽 버튼 땟을시 비활성화
-        Move = FALSE;
-        break;
-
-    case WM_MOUSEMOVE:
-        if (Draw) {         // draw 활성화시 현재좌표를 가져와서 이전 시작점부터 현재좌표까지 사각형 그리기 
-            int Now_X = LOWORD(lParam);
-            int Now_Y = HIWORD(lParam);
+    }
+    else if (uMsg == WM_LBUTTONUP) {
+        if (isDrawing) {
+            // 그림 그리기 모드에서 왼쪽 마우스 버튼 떼면 그림 그리기 종료
+            isDrawing = FALSE;
+            int endX = LOWORD(lParam);
+            int endY = HIWORD(lParam);
             HDC hdc = GetDC(hwnd);
-            SetROP2(hdc, R2_COPYPEN);       // 사각형 색 채워서 만들기
-            Rectangle(hdc, startPoint.x, startPoint.y, Now_X, Now_Y);
-            SetROP2(hdc, R2_NOTXORPEN);     // 이전 사각형 지우기
-            SelectObject(hdc, PinkBrush);
-            Rectangle(hdc, startPoint.x, startPoint.y, Now_X, Now_Y);
+            SelectObject(hdc, whiteBrush);
+            SetROP2(hdc, R2_WHITE);
+            Rectangle(hdc, lastDrawStartX, lastDrawStartY, lastDrawEndX, lastDrawEndY);
+            SetROP2(hdc, R2_COPYPEN);
+            SelectObject(hdc, blueBrush);
+            Rectangle(hdc, startPoint.x, startPoint.y, endX, endY);
+            ReleaseDC(hwnd, hdc);
+            lastDrawStartX = startPoint.x;
+            lastDrawStartY = startPoint.y;
+            lastDrawEndX = endX;
+            lastDrawEndY = endY;
+        }
+    }
+    else if (uMsg == WM_RBUTTONDOWN) {
+        if (LOWORD(lParam) >= lastDrawStartX && LOWORD(lParam) <= lastDrawEndX &&
+            HIWORD(lParam) >= lastDrawStartY && HIWORD(lParam) <= lastDrawEndY) {
+            // 마우스 오른쪽 버튼 클릭 시 그림 이동 모드로 전환
+            isMoving = TRUE;
+            moveOffsetX = LOWORD(lParam) - lastDrawStartX;
+            moveOffsetY = HIWORD(lParam) - lastDrawStartY;
+        }
+    }
+    else if (uMsg == WM_RBUTTONUP) {
+        // 마우스 오른쪽 버튼 떼면 그림 이동 모드 종료
+        isMoving = FALSE;
+    }
+    else if (uMsg == WM_MOUSEMOVE) {
+        if (isDrawing) {
+            // 그림 그리기 모드에서 마우스 이동 시 사각형 그리기
+            int nowX = LOWORD(lParam);
+            int nowY = HIWORD(lParam);
+            HDC hdc = GetDC(hwnd);
+            SetROP2(hdc, R2_COPYPEN);
+            Rectangle(hdc, startPoint.x, startPoint.y, nowX, nowY);
+            SetROP2(hdc, R2_NOTXORPEN);
+            SelectObject(hdc, blueBrush);
+            Rectangle(hdc, startPoint.x, startPoint.y, nowX, nowY);
             ReleaseDC(hwnd, hdc);
         }
-
-        else if (Move && (wParam & MK_RBUTTON)) {   // move 활성화 오른쪽 버튼이 눌려있는 상태에서만 
-            int newX = LOWORD(lParam) - Move_X;     // 현재 마우스 좌표에서 이동 시 시작 지점과의 차이 계산 
-            int newY = HIWORD(lParam) - Move_Y;
-            int width = DrawEnd_X - DrawStart_X;    // 이동중인 사각형의 너비 높이 계산 
-            int height = DrawEnd_Y - DrawStart_Y;
+        else if (isMoving && (wParam & MK_RBUTTON)) {
+            // 그림 이동 모드에서 마우스 이동 시 그림 이동
+            int newX = LOWORD(lParam) - moveOffsetX;
+            int newY = HIWORD(lParam) - moveOffsetY;
+            int width = lastDrawEndX - lastDrawStartX;
+            int height = lastDrawEndY - lastDrawStartY;
             HDC hdc = GetDC(hwnd);
-            SelectObject(hdc, WhiteBrush);
-            SetROP2(hdc, R2_WHITE);                 // 이전 사각형을 지우고 백그라운드 흰색 처리 
-            Rectangle(hdc, DrawStart_X, DrawStart_Y, DrawEnd_X, DrawEnd_Y);
-            SetROP2(hdc, R2_COPYPEN);               // 현재 위치에 사각형 그리기
-            SelectObject(hdc, PinkBrush);
+            SelectObject(hdc, whiteBrush);
+            SetROP2(hdc, R2_WHITE);
+            Rectangle(hdc, lastDrawStartX, lastDrawStartY, lastDrawEndX, lastDrawEndY);
+            SetROP2(hdc, R2_COPYPEN);
+            SelectObject(hdc, blueBrush);
             Rectangle(hdc, newX, newY, newX + width, newY + height);
             ReleaseDC(hwnd, hdc);
-            DrawStart_X = newX;
-            DrawStart_Y = newY;
-            DrawEnd_X = newX + width;
-            DrawEnd_Y = newY + height;
+            lastDrawStartX = newX;
+            lastDrawStartY = newY;
+            lastDrawEndX = newX + width;
+            lastDrawEndY = newY + height;
         }
-        break;
-
-    case WM_DESTROY:    // 윈도우 종료시 프로그램 종료
+    }
+    else if (uMsg == WM_DESTROY) {
+        // 윈도우 파괴 시 종료
         PostQuitMessage(0);
-        break;
-
-    default:            // 사용자 정의한 메시지 처리가 아닌 경우, 기본 창 프로시저를 호출
-        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+    else {
+        // 기본 처리
+        result = DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
 
-    return S_OK;
+    return result;
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow) {    // winmain 함수 부분
-    WNDCLASS wc = { 0 };
-    wc.hInstance = hInstance;
-    wc.lpszClassName = TEXT("Computer Software");
-    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-    wc.hCursor = LoadCursor(NULL, IDC_CROSS);
-    wc.style = CS_HREDRAW | CS_VREDRAW;
-    wc.lpfnWndProc = WindowProc;
+// 진입점
+#ifdef UNICODE
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+#else
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
+#endif
+{
+    WNDCLASS wc;
+    ZeroMemory(&wc, sizeof(wc));
 
-    if (!RegisterClass(&wc)) {
-        MessageBox(NULL, TEXT("윈도우 클래스 등록 실패"), TEXT("에러"), MB_ICONERROR | MB_OK);
-        return -1;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = TEXT("MyDrawingApp"); // 윈도우 클래스 이름
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); // 아이콘
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);   // 커서
+    wc.style = CS_HREDRAW | CS_VREDRAW; // 윈도우 스타일
+    wc.lpfnWndProc = WindowProc; // 윈도우 프로시저 함수 설정
+
+    if (RegisterClass(&wc) == 0) {
+        // 윈도우 클래스 등록 실패 시 종료
+        exit(-1);
     }
 
-    hwnd = CreateWindow(
-        wc.lpszClassName,   // 윈도우 클래스 이름 지정 
-        WINDOW_TITLE,
-        WS_OVERLAPPEDWINDOW & ~(WS_MAXIMIZEBOX | WS_THICKFRAME),    // MAX 최대화 버튼 비활성화, THICK 크기 조절 프레임 비활성화
-        0, 0,   // 초기 시작위치
-        WINDOW_WIDTH, WINDOW_HEIGHT,
-        NULL, NULL, // 부모 윈도우 및 메뉴 NULL 처리
-        hInstance,  // 인스턴스 핸들 전달
-        NULL        // 추가적인 데이터 전달할 포인터, 일반적으로 사용 x
+    HWND hwnd = CreateWindow(
+        wc.lpszClassName,
+        TEXT("2022 "), // 윈도우 제목
+        WS_OVERLAPPEDWINDOW & ~(WS_MAXIMIZEBOX | WS_THICKFRAME), // 윈도우 스타일
+        0, 0, 800, 600, // 윈도우 위치 및 크기
+        NULL, NULL,
+        hInstance,
+        NULL
     );
 
     if (hwnd == NULL) {
-        MessageBox(NULL, TEXT("윈도우 생성 실패"), TEXT("에러"), MB_ICONERROR | MB_OK);
-        return -1;
+        // 윈도우 생성 실패 시 종료
+        exit(-1);
     }
 
-    ShowWindow(hwnd, nCmdShow); //  윈도우창 화면에 표시
+    ShowWindow(hwnd, nCmdShow); // 윈도우 표시
     UpdateWindow(hwnd);
 
-    MSG msg;    // 사용자 입력, 시스템 이벤트 감지 
-    while (GetMessage(&msg, hwnd, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+    MSG msg;
+    ZeroMemory(&msg, sizeof(msg));
+
+    while (msg.message != WM_QUIT) {
+        if (GetMessage(&msg, hwnd, 0, 0)) {
+            TranslateMessage(&msg); // 키보드 입력 번역
+            DispatchMessage(&msg);  // 메시지 디스패치
+        }
     }
 
     return 0;
 }
-
---
+++ VCPP 6주차/VCPP_Work6/VCPP_Work6.c
@@ -0,0 +1,170 @@
+﻿#include <windows.h>
+
+// 전역 변수 선언
+POINT startPoint = { 0 };
+POINT endPoint = { 0 };
+int isKeyPressed = 0;
+
+RECT rect_user = { 5, 5, 10, 10 }; // 사용자 사각형의 초기 위치와 크기
+RECT rect_target = { 50, 50, 150, 150 }; // 목표 사각형의 초기 위치와 크기
+
+// 두 사각형이 겹치는지 확인하는 함수
+int RectIntersect(const RECT* rect1, const RECT* rect2)
+{
+    return rect1->left < rect2->right && rect1->right > rect2->left &&
+        rect1->top < rect2->bottom && rect1->bottom > rect2->top;
+}
+
+// 윈도우 프로시저 함수
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    HDC hdc; // hdc를 직접 선언
+
+    // 브러시 선언 및 초기화
+    HBRUSH hBrush_user = CreateSolidBrush(RGB(0, 0, 255)); // 사용자 사각형 브러시 (파란색)
+    HBRUSH hBrush_target = CreateSolidBrush(RGB(255, 0, 255)); // 목표 사각형 브러시 (보라색)
+    HBRUSH hBrush_eraser = CreateSolidBrush(RGB(255, 255, 255)); // 화면 지우기 브러시 (흰색)
+    const wchar_t* text = L"도착 !!!"; // 출력할 텍스트
+
+    switch (uMsg)
+    {
+    case WM_KEYDOWN:
+        isKeyPressed = 1;
+        if (wParam == VK_RIGHT)
+        {
+            rect_user.left += 1; // 오른쪽 화살표 키 누르면 사용자 사각형을 오른쪽으로 이동
+            rect_user.right += 1;
+        }
+        else if (wParam == VK_LEFT)
+        {
+            rect_user.left -= 1; // 왼쪽 화살표 키 누르면 사용자 사각형을 왼쪽으로 이동
+            rect_user.right -= 1;
+        }
+        else if (wParam == VK_DOWN)
+        {
+            rect_user.top += 1; // 아래쪽 화살표 키 누르면 사용자 사각형을 아래로 이동
+            rect_user.bottom += 1;
+        }
+        else if (wParam == VK_UP)
+        {
+            rect_user.top -= 1; // 위쪽 화살표 키 누르면 사용자 사각형을 위로 이동
+            rect_user.bottom -= 1;
+        }
+        InvalidateRect(hwnd, NULL, TRUE); // 화면을 갱신하기 위한 요청
+        break;
+
+    case WM_PAINT:
+        hdc = GetDC(hwnd); // 화면 그리기를 위해 hdc 얻기
+
+        HBRUSH hOldBrush = (HBRUSH)SelectObject(hdc, hBrush_target);
+        Rectangle(hdc, rect_target.left, rect_target.top, rect_target.right, rect_target.bottom); // 목표 사각형 그리기
+        SelectObject(hdc, hOldBrush);
+
+        HBRUSH hOldBrushUser = (HBRUSH)SelectObject(hdc, hBrush_user);
+        Rectangle(hdc, rect_user.left, rect_user.top, rect_user.right, rect_user.bottom); // 사용자 사각형 그리기
+        SelectObject(hdc, hOldBrushUser);
+
+        if (RectIntersect(&rect_user, &rect_target))
+        {
+            TextOut(hdc, 10, 10, text, lstrlen(text)); // 사용자와 목표 사각형이 겹치면 텍스트 출력
+        }
+
+        ReleaseDC(hwnd, hdc); // hdc 해제
+        break;
+
+    case WM_CLOSE:
+        DestroyWindow(hwnd); // 창 닫기
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0); // 프로그램 종료 메시지 전송
+        break;
+    default:
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+
+    // 사용한 브러시들을 삭제
+    DeleteObject(hBrush_user);
+    DeleteObject(hBrush_target);
+    DeleteObject(hBrush_eraser);
+
+    return S_OK;
+}
+
+#ifdef UNICODE
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
+#else
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR pCmdLine, _In_ int nCmdShow)
+#endif
+{
+    // 윈도우 클래스 선언.
+    WNDCLASS wc;
+    ZeroMemory(&wc, sizeof(wc));    // 모두 0으로 초기화.
+
+    // 윈도우 클래스 값 설정
+    wc.hInstance = hInstance;
+    wc.lpszClassName = TEXT("Computer Software");
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_CROSS);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.lpfnWndProc = WindowProc;
+
+    // 윈도우 클래스 등록.
+    if (RegisterClass(&wc) == 0)
+    {
+        MessageBox(NULL, L"RegisterClass failed!", L"Error", MB_ICONERROR);
+        return -1;  // 예외 처리
+    }
+
+    // Window viewport 영역 조정
+    RECT rect = { 150, 100, 800, 600 };
+    AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, 0);
+    int width = rect.right - rect.left;
+    int height = rect.bottom - rect.top;
+
+    // 윈도우 생성
+    HWND hwnd = CreateWindow(
+        wc.lpszClassName,
+        TEXT("202007032 윤형식"),
+        WS_OVERLAPPEDWINDOW,
+        0, 0,
+        width, height,
+        NULL, NULL,
+        hInstance,
+        NULL
+    );
+
+    // 오류 검사.
+    if (hwnd == NULL)
+    {
+        MessageBox(NULL, L"CreateWindow failed!", L"Error", MB_ICONERROR);
+        exit(-1);
+
+    }
+
+    // 창 보이기.
+    ShowWindow(hwnd, SW_SHOW);  // 창 표시
+    UpdateWindow(hwnd); // 창 업데이트
+
+    // 메시지 처리 루프.
+    MSG msg;
+    ZeroMemory(&msg, sizeof(msg));
+
+    // 메시지 처리.
+    while (msg.message != WM_QUIT)
+    {
+        if (GetMessage(&msg, NULL, 0, 0))
+        {
+            // 메시지 해석
+            TranslateMessage(&msg);
+            // 메시지 처리 함수로 전달
+            DispatchMessage(&msg);
+        }
+    }
+
+    // 윈도우 클래스 등록 해제
+    UnregisterClass(wc.lpszClassName, wc.hInstance);
+
+    // 종료 메시지 반환
+    return (int)msg.wParam;
+}
